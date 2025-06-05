e79e58186bbdfc4ffff7c64264883fe6af80fc0f
Analizabu <coldxmonk@gmail.com>
2025-06-05 22:34:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 const { Client } = require("discord.js-selfbot-v13");
-const client = new Client();
 
+const client = new Client();
 const token = process.env.DISCORD_TOKEN;
+
+if (!token) {
+    console.error("DISCORD_TOKEN is not defined in the .env file.");
+    process.exit(1);
+}
 
 client.on("ready", () => {
     console.log(`${client.user.username} is now online!`);
@@ -11,5 +16,8 @@ client.on("ready", () => {
         status: "online",
     });
 });
+
+client.on("error", console.error);
+client.on("warn", console.warn);
 
 client.login(token);
